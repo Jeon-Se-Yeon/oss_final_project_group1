5e3ffc8f31ecedd3455338c3e6692522dd937b7a
yoonseongeon-dev <yoonseongeon@gmail.com>
2025-12-02 17:49:03 +0900

    create react project

--- anime-demo/src/App.js
+++ anime-demo/src/App.js
@@ -12,19 +12,20 @@ import MyPage from "./pages/MyPage"; // 신규 페이지
 // 스타일 및 상수 파일은 컴포넌트 내부에서 임포트하여 사용
 
 function App() {
-    return (
-        <AuthProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/signup" element={<SignupPage />} />
-                    <Route path="/detail/:id" element={<Detail />} />
-                    <Route path="/mypage" element={<MyPage />} /> {/* 마이페이지 라우트 추가 */}
-                </Routes>
-            </BrowserRouter>
-        </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/mypage" element={<MyPage />} />{" "}
+          {/* 마이페이지 라우트 추가 */}
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
 export default App;
